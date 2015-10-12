fdcad1a9c131eb24ccd81ab3865daee5f1be2c6c
amit <amitjipatil@gmail.com>
2015-10-12 19:46:39 +0530

    file upload project

---
+++ fileUploadServlet/src/fileUploadServlet/Success.java
@@ -0,0 +1,14 @@
+package fileUploadServlet;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import java.io.*;
+public class Success extends HttpServlet{
+	
+	@Override
+	public void service(ServletRequest arg0, ServletResponse arg1)
+			throws ServletException, IOException {
+		
+	}
+
+}
---
+++ fileUploadServlet/src/fileUploadServlet/UploadData.java
@@ -0,0 +1,34 @@
+package fileUploadServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
+
+import javazoom.upload.MultipartFormDataRequest;
+import javazoom.upload.UploadBean;
+
+import javax.servlet.http.*;
+import javax.servlet.*;
+public class UploadData extends HttpServlet{
+	public void doPost(HttpServletRequest req,HttpServletResponse res)throws ServletException, IOException
+	{
+		//System.out.println("--------------1------------");
+		PrintWriter pw=res.getWriter();
+		try
+		{
+			UploadBean upb=new UploadBean();
+			upb.setFolderstore("d:/store");
+			upb.setOverwrite(false);
+			MultipartFormDataRequest mreq=new MultipartFormDataRequest(req);
+			upb.store(mreq);
+			// it completes file uploading
+			RequestDispatcher rd=req.getRequestDispatcher("uploadSuccess.jsp");
+			rd.forward(req, res);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+}
+ 
