d770b795963b9ec6f44f6ae9742bf8e977f08cb0
amit <amitjipatil@gmail.com>
2015-10-20 18:50:24 +0530

    primary servlet file

---
+++ jspServletMiniProject/src/in/hybridsoft/facebook/servlet/PrimaryServlet.java
@@ -0,0 +1,30 @@
+package in.hybridsoft.facebook.servlet;
+import in.hybridsoft.facebook.service.AddNewEmailServiceaImpl;
+
+import java.io.IOException;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+public class PrimaryServlet extends HttpServlet{
+
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+	
+		RequestDispatcher rd=null;
+		int id=Integer.parseInt(req.getParameter("id"));
+		int uid=Integer.parseInt(req.getParameter("uid"));
+
+	  AddNewEmailServiceaImpl addNewEmailServiceaImpl=new AddNewEmailServiceaImpl();
+	 boolean bool=addNewEmailServiceaImpl.makePrimary(id,uid);
+	 if(bool)
+	 {
+		 rd=req.getRequestDispatcher("/jsp/changeemail.jsp");
+		 rd.forward(req, res);
+		 
+	 }
+	 
+	}
+	
+}
