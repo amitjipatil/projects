4581d5134ce6689280e096beb57e0929769bedb6
amit <amitjipatil@gmail.com>
2016-12-09 00:15:12 +0530

    na

--- fpsAutomation/src/main/java/com/barclaycard/bPaid/app/main/ReadFile.java
+++ fpsAutomation/src/main/java/com/barclaycard/bPaid/app/main/ReadFile.java
@@ -7,19 +7,19 @@ import java.io.IOException;
 
 public class ReadFile {
 	public static void main(String[] args) {
-		try {
-			BufferedReader in = new BufferedReader(new FileReader("D:/NPKey.txt"));
+		
+			
 			try {
-				in.readLine();
+				BufferedReader in = new BufferedReader(new FileReader("D:/NPKey.txt"));
+				String line=in.readLine();
+				line.substring(10,13);
+				System.out.println(line);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			
-		} catch (FileNotFoundException e) {
-			
-			e.printStackTrace();
-		}
+	
 	}
 
 }
