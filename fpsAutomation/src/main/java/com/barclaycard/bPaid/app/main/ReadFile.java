4cbad7a37fbf691f5d4534497cd3420732e64fb1
amit <amitjipatil@gmail.com>
2016-12-12 01:38:53 +0530

    commented

--- fpsAutomation/src/main/java/com/barclaycard/bPaid/app/main/ReadFile.java
+++ fpsAutomation/src/main/java/com/barclaycard/bPaid/app/main/ReadFile.java
@@ -12,31 +12,42 @@ public class ReadFile {
 	public static void main(String[] args) {
 			try {
 				int count=0;
+				//read file
 				BufferedReader in = new BufferedReader(new FileReader("D:/testFile.txt"));
+				// read line
 				String line=in.readLine();
-				String t=line.substring(1,5);//1 nantar 5 paryant
+				//String t=line.substring(1,5);//1 nantar 5 paryant
+				// find transactions using | (pipe)
 				String []arr=line.split("\\|");
+				// perform operation on each transactions
 				for(String txn:arr)
 				{
+					//skip  header an footer part
 					if(txn.startsWith("9200"))
 					{
 						System.out.println(txn);
-					String hex;
-					hex=txn.substring(4,20);
+					//find hex and convert to binary
+					String binary=BinaryConverter.hexToBin(txn.substring(4,20));
+					//System.out.println("binary=="+binary);
 					
-					System.out.println("hexadecimal=="+hex);
-					String binary=BinaryConverter.hexToBin(hex);
-					System.out.println("binary=="+binary);
+					//find the bit position
 					char[] chars = binary.toCharArray();
 					for(char c:chars)
 					{
 						++count;
+						//check whether the position is one or zero
 						if(Character.getNumericValue(c)==1)
 						{
-							
+							// find the bit name bits start_position and bits end_position
+							System.out.println("count==="+count);
 							String bitValue=BitMapping.getMapping(count);
-						System.out.println("count###=="+count);
+							System.out.println("bit value="+bitValue);
+						
 						String value[]=bitValue.split(" ");
+						for(String s:value)
+						{
+							System.out.println("@@@@@@@@@@@@@@"+s);
+						}
 						
 						
 						
--- fpsAutomation/src/main/java/com/barclaycard/bPaid/app/utility/BitMapping.java
+++ fpsAutomation/src/main/java/com/barclaycard/bPaid/app/utility/BitMapping.java
@@ -4,8 +4,10 @@ import java.util.HashMap;
 
 public class BitMapping {
 	static HashMap<Integer,String> hm;
-	public static String getMapping(int key)
+	public static String getMapping(int key1)
 	{
+		hm=new HashMap<Integer, String>();
+		
 		hm.put(1,"BitMapSecondary 21 37");
 		hm.put(2,"ProcessingCode 37 43");
 		hm.put(4,"OriginalAmount 43 57");
@@ -33,11 +35,42 @@ public class BitMapping {
 		hm.put(31,"temp 25 30");
 		hm.put(32,"temp 25 30");
 		hm.put(33,"temp 25 30");
+		hm.put(34,"temp 25 30");
+		hm.put(35,"temp 25 30");
+		hm.put(36,"temp 25 30");
+		hm.put(37,"temp 25 30");
+		hm.put(38,"temp 25 30");
+		hm.put(39,"temp 25 30");
+		hm.put(40,"temp 25 30");
+		hm.put(41,"temp 25 30");
+		hm.put(42,"temp 25 30");
+		hm.put(43,"temp 25 30");
+		hm.put(44,"temp 25 30");
+		hm.put(45,"temp 25 30");
+		hm.put(46,"temp 25 30");
+		hm.put(47,"temp 25 30");
+		hm.put(48,"temp 25 30");
+		hm.put(49,"temp 25 30");
+		hm.put(50,"temp 25 30");
+		hm.put(51,"temp 25 30");
+		hm.put(52,"temp 25 30");
+		hm.put(53,"temp 25 30");
+		hm.put(54,"temp 25 30");
+		hm.put(55,"temp 25 30");
+		hm.put(56,"temp 25 30");
+		hm.put(57,"temp 25 30");
+		hm.put(58,"temp 25 30");
+		hm.put(59,"temp 25 30");
+		hm.put(60,"temp 25 30");
+		hm.put(61,"temp 25 30");
+		hm.put(62,"temp 25 30");
+		hm.put(63,"temp 25 30");
+		hm.put(64,"temp 25 30");
 		
 		
 		
 		
-		return null;
+		return hm.get(key1);
 		
 	}
 
